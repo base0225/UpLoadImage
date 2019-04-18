d53d30c353f1bad2c9134e9326c7eb5535988a17
zhujia <zhujia@zhujiadeMacBook-Pro-3.local>
2019-04-18 23:10:01 +0800

    [Beth]modify || 增加文件

---
+++ UpLoadImage/UpLoadImage/View/ZJDynamicImageAddCell.h
@@ -0,0 +1,19 @@
+//
+//  ZJDynamicImageAddCell.h
+//  UpLoadImage
+//
+//  Created by zhujia on 2019/4/18.
+//  Copyright © 2019 朱佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJDynamicImageAddCell : UICollectionViewCell
+
+- (void)bindmodelwithstr:(NSString *)str;
+
+@end
+
+NS_ASSUME_NONNULL_END
